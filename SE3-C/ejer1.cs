72078227c1c243abb403e1d648702dd55747ae99
Vortex2017-php <Nexark0212@gmail.com>
2026-04-10 10:23:33 -0500

    Subiendo ejer1 en c#

---
+++ SE3-C/ejer1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejer1
+    {
+        static void Main(string[] args)
+        {
+            String N, C;
+            Console.Write("\nIngrese nombre: ");
+            N = Console.ReadLine(); 
+            Console.Write("\nIngrese carrera: ");
+            C = Console.ReadLine();
+
+            Console.WriteLine("\n-------------- IMPRESIÓN CON + --------------");
+            Console.WriteLine(N+", Bienvenido al curso de Fundamentos de Algoritmo " + C);
+
+            Console.WriteLine("\n-------------- IMPRESIÓN CON $ --------------");
+            Console.WriteLine($"{N}, Bienvenido al curso de Fundamentos de Algoritmo {C}");
+
+            Console.ReadKey();
+        }
+    }
+}
