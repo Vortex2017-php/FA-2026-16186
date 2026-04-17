dda1a9c849bf45dc199818fcb7738b2ee8d46a28
Vortex2017-php <nexark0212@gmail.com>
2026-04-17 10:17:28 -0500

    Update ejer5.cs

--- SE3-C/ejer5.cs
+++ SE3-C/ejer5.cs
@@ -1,16 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SE3_C
 {
-    internal class ejer5
+    internal class eje5
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            Console.Write("Ingrese un numero: ");
+            string num = Console.ReadLine();
+            int entero = int.Parse(num); ;
+            double deci = double.Parse(num);
 
+            Console.WriteLine($"\nRestos: {entero % 2}");
+            Console.WriteLine($"Dividido entre 3: {deci / 3}");
         }
     }
 }
