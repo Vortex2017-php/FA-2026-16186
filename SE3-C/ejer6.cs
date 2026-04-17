c39e957271f3bec65a25b042f41524848b9af63c
Vortex2017-php <nexark0212@gmail.com>
2026-04-17 10:17:04 -0500

    Update ejer6.cs

--- SE3-C/ejer6.cs
+++ SE3-C/ejer6.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace SE3_C
 {
-    internal class ejer6
+    internal class eje6
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese los segundos: ");
+            int se = int.Parse(Console.ReadLine());
 
+            int horas = se / 3600;
+            int min = (se % 3600) / 60;
+            int resto = se % 60;
+
+            Console.WriteLine($"Horas: {horas}\nMinutos: {min}\nResto: {resto}");
         }
     }
 }
