69f2099630ca367e52c90b69887c780f390c70de
Vortex2017-php <Nexark0212@gmail.com>
2026-04-24 10:34:17 -0500

    Subiendo ejercicios de la semana 4 (24-04-2026)

---
+++ SE4-C/ejer2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE4_C
+{
+    internal class ejer2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el año: ");
+            int an = int.Parse(Console.ReadLine());
+
+            if ((an % 4 == 0 & an % 100 != 0) || an % 400 == 0)
+            {
+                Console.WriteLine("\nEl año es bisiesto");
+            }
+            else Console.WriteLine("\nEl año no es bisiesto");
+
+            if (an % 2 == 0) Console.WriteLine("\nEl año es par");
+            else Console.WriteLine("\nEl año es impar");
+        }
+    }
+}
---
+++ SE4-C/ejer3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE4_C
+{
+    internal class ejer3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------SISTEMA DE CONVERSIÓN MONETARIO--------");
+            Console.Write("Ingrese el monto en soles: ");
+            double moneda = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n1. Convertir a dolares");
+            Console.WriteLine("2. Convertir a a euros\n");
+
+
+            Console.WriteLine("Ingrese una opción");
+            int opciones = int.Parse(Console.ReadLine());
+
+            switch (opciones)
+            {
+                case 1:
+                    double dolares = moneda / 3.75;
+                    Console.WriteLine("\nDolares: " + dolares);
+                    break;
+                case 2:
+                    double euros = moneda / 4.05;
+                    Console.WriteLine("\nEuros: " + euros);
+                    break;
+                default:
+                    Console.WriteLine("Opción incorrecta");
+                    break;
+            }
+
+
+        }
+    }
+}
